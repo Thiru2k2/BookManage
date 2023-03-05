52fa2ac37e1403e20bc86b4c817ac51ae38aae29
Thirumoorthi A M <123548146+Thiru2k2@users.noreply.github.com>
2023-03-05 18:38:53 +0530

    Add files via upload

---
+++ BookServiceInterface.java
@@ -0,0 +1,15 @@
+package com.thiru;
+
+import java.util.*;
+public interface BookServiceInterface {
+    void addBook() ;
+
+    void showAllBooks();
+
+    void showAllAvailableBooks();
+
+    void borrowBook();
+
+    void returnBook();
+}
+
